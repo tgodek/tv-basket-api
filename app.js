9072e91735adec1ec277d17348142e22f468639a
vljukap98 <30662155+vljukap98@users.noreply.github.com>
2021-02-17 19:42:41 +0100

    Database connection
    Created connection to database, reversed mongoose version to 5.11.15, handling error from being unable to connect

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const express = require('express');
+require('dotenv/config');
 
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    {useNewUrlParser: true, useUnifiedTopology: true}
+);
+
+var db = mongoose.connection;
+
+db.once('open', () => {
+    console.log("Connected to DB");
+});
+
+db.on('error', console.error.bind(console, 'Error connecting to database'));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,
+  console.log(`Server running mode on port ${PORT}`)
+);
+
 
 
 //rute
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('Hello 1');
 });
 
 app.listen(3000);
