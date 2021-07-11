726c0d9473c50a34012bb4c68fbdcf7944a8b01f
Tomislav Godek <tomislav8462@gmail.com>
2021-07-11 19:02:40 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const userController = require("../controllers/userController");
+const authController = require("../middleware/auth");
 
 const router = Router();
 
 router.get("/", userController.all_users_get);
+router.get("/me", authController.verifyToken, userController.me_get);
 router.post("/register", userController.register_post);
 router.post("/login", userController.login_post);
 
