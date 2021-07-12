75b63df3c558432fb84a911770b30d9aa83fb437
Tomislav Godek <tomislav8462@gmail.com>
2021-07-12 17:38:44 +0200

    Updated .env variable

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports.verifyToken = (req, res, next) => {
   if (!token) return res.status(401).send("Access Denied");
 
   try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    const verified = jwt.verify(token, process.env.AC_TOKEN_SECRET);
     req.user = verified.id;
     next();
   } catch (err) {
