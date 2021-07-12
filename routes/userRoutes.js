f1e9c0e9f6f0945f455d032b1f3c9df51c644c35
Tomislav Godek <tomislav8462@gmail.com>
2021-07-12 17:41:56 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const authController = require("../middleware/auth");
 const router = Router();
 
 router.get("/", userController.all_users_get);
+router.get("/revoke_refresh_token", userController.revokeRefreshTokenForUser);
 router.get("/me", authController.verifyToken, userController.me_get);
 router.post("/register", userController.register_post);
 router.post("/login", userController.login_post);
