c1b1b4ab36acb59fe004276ad54832d70cae2326
Luka Jaković <30662155+vljukap98@users.noreply.github.com>
2021-07-17 19:07:38 +0200

    Added new functions and routes
    New functions and routes are for adding movies to watchlist and marking them as watched

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,6 +1,7 @@
-//const Movie = require("../models/Movie");
-
 require("dotenv").config();
+const Movie = require("../models/Movie");
+const User = require("../models/User");
+const userController = require("../controllers/userController");
 const MovieDB = require("node-themoviedb");
 
 const mdb = new MovieDB(process.env.API_KEY)
@@ -71,6 +72,32 @@ module.exports.movie_search = async (req, res) => {
     };
     const discoverMovies = await mdb.search.movies(args);
     res.json(discoverMovies);
+  } catch (e) {
+    res.send(e);
+  }
+};
+
+module.exports.movie_add_to_watchlist = async (req, res) => {
+  try {
+    var loggedIn = await User.find({ _id: req.body.user});
+    var movie = req.body.movie;
+
+  } catch (e) {
+    res.send(e);
+  }
+};
+
+module.exports.movie_add_to_tracked = async (req, res) => {
+  try {
+    const me = await User.findOne({ _id: req.body.userId });
+    const movie = req.body.movieId;
+
+    //TODO check if movie is already in the list
+    me.trackedMovies.push(movie);
+    me.save(function (err, result) {
+      if(err) res.send(err);
+      else res.send("Movie marked as watched!");
+    });
   } catch (e) {
     res.send(e);
   }
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -8,6 +8,9 @@ router.get("/", authController.verifyToken, movieController.discover_movies_get)
 router.get("/top", authController.verifyToken, movieController.top_movies_get);
 router.get("/popular", authController.verifyToken, movieController.popular_movies_get);
 router.get("/info", authController.verifyToken, movieController.movie_info);
-router.get("/search", /*authController.verifyToken,*/ movieController.movie_search);
+router.get("/search", authController.verifyToken, movieController.movie_search);
+router.post("/add_to_watchlist", authController.verifyToken, movieController.movie_add_to_watchlist);
+router.post("/add_to_tracked", authController.verifyToken, movieController.movie_add_to_tracked);
+
 
 module.exports = router;
